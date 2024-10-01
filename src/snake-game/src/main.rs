71f7e9d3176c064f0abdca036972bca573676e5d
Andrii Demydenko <worm2fed@gmail.com>
2024-10-01 22:56:30 +0100

    feat(snake-game): use non-blocking display

--- src/snake-game/src/main.rs
+++ src/snake-game/src/main.rs
@@ -2,11 +2,13 @@
 #![no_std]
 
 mod controls;
+mod display;
 mod game;
 
 use cortex_m_rt::entry;
+use display::clear_display;
 use microbit::{
-    display::blocking::Display,
+    display::nonblocking::{BitImage, GreyscaleImage},
     hal::{prelude::*, Rng, Timer},
     Board,
 };
@@ -14,36 +16,38 @@ use panic_rtt_target as _;
 use rtt_target::rtt_init_print;
 
 use crate::controls::{get_turn, init_buttons};
+use crate::display::{display_image, init_display};
 use crate::game::{Game, GameStatus};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0);
+    let mut timer = Timer::new(board.TIMER0).into_periodic();
     let mut rng = Rng::new(board.RNG);
     let mut game = Game::new(rng.random_u32());
-    let mut display = Display::new(board.display_pins);
 
     init_buttons(board.GPIOTE, board.buttons);
+    init_display(board.TIMER1, board.display_pins);
+
     loop {
-        // Game loop
         loop {
-            let image = game.game_matrix(9, 9, 9);
-            // The brightness values are meaningless at the moment as we haven't yet
-            // implemented a display capable of displaying different brightnesses
-            display.show(&mut timer, image, game.step_len_ms());
+            // Game loop
+            let image = GreyscaleImage::new(&game.game_matrix(6, 3, 9));
+            display_image(&image);
+            timer.delay_ms(game.step_len_ms());
             match game.status {
-                // Placeholder as we haven't implemented controls yet
                 GameStatus::Ongoing => game.step(get_turn(true)),
                 _ => {
                     for _ in 0..3 {
-                        display.clear();
+                        clear_display();
                         timer.delay_ms(200u32);
-                        display.show(&mut timer, image, 200);
+                        display_image(&image);
+                        timer.delay_ms(200u32);
                     }
-                    display.clear();
-                    display.show(&mut timer, game.score_matrix(), 1000);
+                    clear_display();
+                    display_image(&BitImage::new(&game.score_matrix()));
+                    timer.delay_ms(2000u32);
                     break;
                 }
             }
